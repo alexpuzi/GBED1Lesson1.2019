3240676dc7297d421a46168a84326cdb59cd6daf
alexpuzi <alex.puzi@gmail.com>
2018-09-21 11:37:56 +0300

    изменил переменные

--- 12_24/src/com/company/Main.java
+++ 12_24/src/com/company/Main.java
@@ -1,8 +1,12 @@
+
 package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("WAAGHHHHHHHH!");// write your code here
+	int saddam = 1234;
+	int qwer = 12324;
+	int dfg = saddam + qwer;
+        System.out.println(dfg);// write your code here
     }
 }
