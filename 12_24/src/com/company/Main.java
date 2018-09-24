eb12cbba64c9e7af95a1a7ecec3337909458e0e5
alexpuzi <alex.puzi@gmail.com>
2018-09-24 13:08:51 +0300

    Добавил переменные

--- 12_24/src/com/company/Main.java
+++ 12_24/src/com/company/Main.java
@@ -1,12 +1,25 @@
-
 package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
-	int saddam = 1234;
-	int qwer = 12324;
-	int dfg = saddam + qwer;
-        System.out.println(dfg);// write your code here
+        int a = 12;
+        byte number1 = 14;
+        short number2 = 100;
+        String title = "SDK";
+        char letter = 'M';
+        double sum = 16878.3245;
+        float pi = 3.14f;
+        boolean result = false;
+        System.out.println(a);
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(title);
+        System.out.println(letter);
+        System.out.println(sum);
+        System.out.println(pi);
+        System.out.println(result);
+
+
     }
 }
