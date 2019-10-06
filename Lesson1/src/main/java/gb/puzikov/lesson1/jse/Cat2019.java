9eda7763bb31f4ee5579f950733e7bf6c8fcb690
alexpuzi <alex.puzi@gmail.com>
2019-10-06 11:14:36 +0300

    Start do homework

---
+++ Lesson1/src/main/java/gb/puzikov/lesson1/jse/Cat2019.java
@@ -0,0 +1,10 @@
+package gb.puzikov.lesson1.jse;
+
+public class Cat2019 {
+    String jump2019;
+    String run2019;
+
+    void printInfo() {
+        System.out.println(jump2019 + "\n" + run2019);
+    }
+}
