6bc0bcce9652f53f8c3d169efdd0e0dae4434a8c
alexpuzi <alex.puzi@gmail.com>
2019-10-21 15:20:42 +0300

    переделал part with Thread

--- GEEK BRAINS 2019/Ed1Lesson5/src/puzikovGeekrais/lesson5/MyThread.java
+++ GEEK BRAINS 2019/Ed1Lesson5/src/puzikovGeekrais/lesson5/MyThread.java
@@ -1,58 +1,32 @@
 package puzikovGeekrais.lesson5;
 
+import java.lang.Math;
+
 public class MyThread extends Thread {
     static final int sIze = 10000000;
     static final int hAlf = sIze / 2;
-    float[] arrHM4;
-    float[] arrHM4_1;
-    float[] arrHM4_2;
+    float[] arrHM1 = new float[sIze];
+    float[] arrHM2 = new float[hAlf];
+    float[] arrHM3 = new float[hAlf];
 
-    public MyThread() {
-        arrHM4 = new float[sIze];
-    }
+    public void runFirst() {
 
-    public void myThread() {
+
         for (int i = 0; i < sIze; i++) {
-            arrHM4[i] = 1;
-//            System.out.println(arrHM4[i]);//проверка вывод единицы
-
-            //
+            arrHM1[i] = (float) (arrHM1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-
-    }long a = System.currentTimeMillis();
-
-    public float[] calculateMetod(float[] arrHM4) {
-        for (int i = 0; i < arrHM4.length; i++)
-
-            arrHM4[i] = (float) (arrHM4[i] * Math.sin(0.2f + arrHM4[i] / 5) * Math.cos(0.2f + arrHM4[i] / 5) * Math.cos(0.4f + arrHM4[i] / 2));
-
-        return arrHM4;
+        long a = System.currentTimeMillis();
+        System.out.println(a);
     }
 
-    public void run1() {
-        float[] arrHM4_1 = new float[sIze];
-        System.arraycopy(arrHM4, 0, arrHM4_1, 0, hAlf);
-        System.arraycopy(arrHM4,hAlf,arrHM4_2,0,hAlf);
-        new Thread(){
-            @Override
-            public void run() {
-                super.run();
-                float[] a1 = calculateMetod(arrHM4_1);
-                System.arraycopy(a1,0,arrHM4_1,0,a1.length);
-            }
-        }.start();
-        new Thread(){
-            @Override
-            public void run() {
-                super.run();
-                float[] a2 = calculateMetod(arrHM4_2);
-                System.arraycopy(a2,0,arrHM4_2,0,a2.length);
-            }
-        }.start();
-        System.arraycopy(arrHM4_1,0,arrHM4,0,hAlf);
-        System.arraycopy(arrHM4_2,0,arrHM4,hAlf,hAlf);
-        System.out.println("Tow threads ends with:"+(System.currentTimeMillis()-a));
-            }
-
-
+    @Override
+    public void run() {
+        System.arraycopy(arrHM1,hAlf,arrHM2,0,hAlf);
+        System.arraycopy(arrHM1,hAlf,arrHM3,0,hAlf);
+        long a = System.currentTimeMillis();
+        System.out.println(a);
+        System.arraycopy(arrHM2,0,arrHM1,hAlf,hAlf);
+    }
 }
+
+
