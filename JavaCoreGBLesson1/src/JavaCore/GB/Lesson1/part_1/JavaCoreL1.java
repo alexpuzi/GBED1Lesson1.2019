05ae23bb78cddef03df336bc87fc7d0238612b7b
alexpuzi <alex.puzi@gmail.com>
2019-11-13 11:18:29 +0300

    begin

---
+++ JavaCoreGBLesson1/src/JavaCore/GB/Lesson1/part_1/JavaCoreL1.java
@@ -0,0 +1,15 @@
+package JavaCore.GB.Lesson1.part_1;
+
+
+public class JavaCoreL1<T> {
+    private T number1;
+
+
+    public JavaCoreL1(T number_1) {
+        this.number1 = number_1;
+    }
+
+    public T getNumber1() {
+        return number1;
+    }
+}
---
+++ JavaCoreGBLesson1/src/JavaCore/GB/Lesson1/part_2/JavaCoreL1P2.java
@@ -0,0 +1,18 @@
+package JavaCore.GB.Lesson1.part_2;
+
+public class JavaCoreL1P2 <T> {
+    T obj_1;
+    T obj_2;
+    T obj_3;
+    private T[] words = new<T> {obj_1,obj_2,obj_1};
+
+    public JavaCoreL1P2(T[] words) {
+        this.words = words;
+    }
+    public JavaCoreL1P2(T obj_1, T obj_2, T obj_3) {
+        this.obj_1 = obj_1;
+        this.obj_2 = obj_2;
+        this.obj_3 = obj_3;
+    }
+}
+
